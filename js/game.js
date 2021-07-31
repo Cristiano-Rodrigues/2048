a7366a9f01d5901a4aa09ef27e514d2199672183
Cristiano Rodrigues <64600779+Cristiano-Rodrigues@users.noreply.github.com>
2021-07-31 18:19:56 +0100

    blocos aleatórios
    Criei a lógica de adicionar dois blocos aleatoriamente no início do jogo

--- js/game.js
+++ js/game.js
@@ -1,7 +1,34 @@
 function Game(wrapper, Display) {
-  this.grid = new Grid(4);
+  this.setup();
   this.display = new Display(wrapper, this.grid);
 
   this.display.drawScreen();
   this.display.drawBlocks();
+}
+
+Game.prototype.setup = function() {
+  const previousState = this.getState();
+
+  if (previousState) {
+    this.grid      = new Grid(4, previousState.grid.cells);
+    this.score     = previousState.score;
+    this.bestScore = previousState.bestScore;
+    this.status    = previousState.status;
+  } else {
+    this.grid      = new Grid(4);
+    this.score     = this.bestScore = 0;
+    this.status    = null;
+    this.addStartBlocks();
+  }
+}
+
+Game.prototype.getState = function() {
+  return;
+}
+
+Game.prototype.addStartBlocks = function() {
+  const startBlocks = 2;
+
+  for (let i = 0; i < startBlocks; i++)
+    this.grid.addRandomBlock();
 }
--- js/grid.js
+++ js/grid.js
@@ -1,16 +1,15 @@
 function Grid(size, cells) {
   this.size = size;
   this.cells = (cells && this.normalizeCells(cells))
-                    || this.getEmptyCells();
-  this.cells[0][0] = new Block(new Vector(0, 0));
+                    || this.getEmptyGrid();
 }
 
-Grid.prototype.getEmptyCells = function() {
-  const emptyCells = [];
+Grid.prototype.getEmptyGrid = function() {
+  const emptyGrid = [];
   for (let y = 0; y < this.size; y++)
-    emptyCells.push(new Array(this.size).fill(0))
+    emptyGrid.push(new Array(this.size).fill(null));
 
-  return emptyCells;
+  return emptyGrid;
 }
 
 Grid.prototype.each = function(action, context) {
@@ -26,3 +25,34 @@ Grid.prototype.each = function(action, context) {
 Grid.prototype.getContent = function(position) {
   return this.cells[position.y][position.x];
 }
+
+Grid.prototype.addRandomBlock = function() {
+  const space = this.getRandomEmptyCell();
+
+  this.setContent(space, new Block(space));
+}
+
+Grid.prototype.getRandomEmptyCell = function() {
+  const emptyCells = this.getEmptyCells();
+
+  return emptyCells[ Math.floor(Math.random() *
+                        emptyCells.length) ];
+}
+
+Grid.prototype.getEmptyCells = function() {
+  const emptyCells = [];
+
+  for (let y = 0; y < this.size; y++) {
+    for (let x = 0; x < this.size; x++) {
+      const cell = this.getContent(new Vector(x, y));
+      if (cell === null)
+        emptyCells.push(new Vector(x, y));
+    }
+  }
+
+  return emptyCells;
+}
+
+Grid.prototype.setContent = function(position, value) {
+  this.cells[position.y][position.x] = value;
+} 
